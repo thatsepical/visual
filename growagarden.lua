5da213ffd2a02bc5c6830ee4553483ddd66916a0
thatsepical <zenxqrblx@gmail.com>
2025-06-21 08:16:50 +0800

    Update growagarden.lua

--- growagarden.lua
+++ growagarden.lua
@@ -3,9 +3,6 @@ local backpack = player:WaitForChild("Backpack")
 local playerGui = player:WaitForChild("PlayerGui")
 local UIS = game:GetService("UserInputService")
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/ataturk123/GardenSpawner/refs/heads/main/Spawner.lua"))()
-getgenv().Executed = nil
-
 local screenGui = Instance.new("ScreenGui", playerGui)
 screenGui.Name = "PetSpawnerUI"
 screenGui.ResetOnSpawn = false
